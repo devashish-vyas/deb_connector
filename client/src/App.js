913bfb3780f7d825928467ac26f52668aead6673
devashish vyas <devashishvyas77@gmail.com>
2022-08-07 19:33:01 +0530

    started working on redux

--- client/src/App.js
+++ client/src/App.js
@@ -4,15 +4,26 @@ import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
+import Alert from "./components/layout/Alert";
 import "./App.css";
+//Redux
+import {Provider} from 'react-redux';
+
+import store from './store';
+
+
 
 const App = () => {
   return (
-    <>
+    <Provider store={store}>
       <Router>
         <Navbar />
+        <Alert />
+      
+
         <Routes>
-          <Route path="/" element={<Landing />} />
+          
+        <Route path="/" element={<Landing />} />
           <Route
             path="/register"
             element={
@@ -31,7 +42,8 @@ const App = () => {
           />
         </Routes>
       </Router>
-    </>
+      </Provider>
+    
   );
 };
 
